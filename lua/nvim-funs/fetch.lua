1a5b77aeebaa9ae6ac10141df9283f0d8abc8029
rjm <robjmass@gmail.com>
2024-04-05 14:50:23 -0400

    popup win fix

--- lua/nvim-funs/fetch.lua
+++ lua/nvim-funs/fetch.lua
@@ -22,11 +22,18 @@ function M.popup()
         end
     end
 
+    vim.api.nvim_win_set_option(popup_win, "wrap", false)  -- Disable line wrapping
+    vim.api.nvim_win_set_option(popup_win, "cursorline", false)  -- Disable cursorline
+    vim.api.nvim_win_set_option(popup_win, "cursorcolumn", false)  -- Disable cursorcolumn
+
+    vim.api.nvim_buf_set_keymap(popup_win, "n", "<Esc>", "<Cmd>q<CR>", { noremap = true })
+    vim.api.nvim_buf_set_keymap(popup_win, "n", "q", "<Cmd>q<CR>", { noremap = true })
+
     local popup_buf = vim.api.nvim_win_get_buf(popup_win)
     vim.api.nvim_buf_attach(popup_buf, false, {
-        on_detach = function()
-            vim.api.nvim_buf_detach(popup_buf)
-        end,
+        --on_detach = function()
+        --    vim.api.nvim_buf_detach(popup_buf)
+        --end,
         on_input = function(_, input)
             on_keypress(input)
         end,
