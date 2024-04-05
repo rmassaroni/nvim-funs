9d7763bc62463a51d0103709970d4ecf723ec807
rjm <robjmass@gmail.com>
2024-04-05 15:25:03 -0400

    unnamed commit

--- lua/nvim-funs/fetch.lua
+++ lua/nvim-funs/fetch.lua
@@ -3,10 +3,14 @@ local M = {}
 --local popup_content = "Hello world"
 
 function M.popup()
-    local popup_win = vim.api.nvim_open_win(0, true, {
+    local popup_buf = vim.api.nvim_create_buf(false, true)
+    vim.api.nvim_buf_set_lines(popup_buf, 0, 1, false, {"Hello world"})
+
+    --local popup_win = vim.api.nvim_open_win(0, true, {
+    local popup_win = vim.api.nvim_open_win(popup_buf, true, {
         relative = "cursor",
-        width = 12,
-        height = 1,
+        width = 120,
+        height = 20,
         row = 1,
         col = 1,
         style = "minimal",
@@ -14,7 +18,7 @@ function M.popup()
     })
 
     --vim.api.nvim_buf_set_lines(popup_win, 0, 1, false, {"Hello world"})
-    vim.api.nvim_buf_set_lines(vim.api.nvim_win_get_buf(popup_win), 0, 1, false, {"Hello world"})
+    --vim.api.nvim_buf_set_lines(vim.api.nvim_win_get_buf(popup_win), 0, 1, false, {"Hello world"})
 
     local function on_keypress(key)
         if key == "q" or key == "<Esc>" then
@@ -32,12 +36,13 @@ function M.popup()
     vim.api.nvim_buf_set_keymap(vim.api.nvim_win_get_buf(popup_win), "n", "<Esc>", "<Cmd>q<CR>", { noremap = true })
     vim.api.nvim_buf_set_keymap(vim.api.nvim_win_get_buf(popup_win), "n", "q", "<Cmd>q<CR>", { noremap = true })
 
-    local popup_buf = vim.api.nvim_win_get_buf(popup_win)
+    --local popup_buf = vim.api.nvim_win_get_buf(popup_win)
     vim.api.nvim_buf_attach(popup_buf, false, {
         --on_detach = function()
         --    vim.api.nvim_buf_detach(popup_buf)
         --end,
-        on_input = function(_, input)
+        --on_input = function(_, input)
+        on_input = function(input)
             on_keypress(input)
         end,
     })
